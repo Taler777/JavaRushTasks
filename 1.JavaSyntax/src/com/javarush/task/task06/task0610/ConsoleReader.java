c0e22d9621418ca3685f5fbec4b74a540b4abdc3
Alexey <taler2002@gmail.com>
2018-10-23 21:32:51 +0300

    JavaRushTasks

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0610/ConsoleReader.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task06.task0610;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Класс ConsoleReader
+*/
+
+public class ConsoleReader {
+
+    public static String readString() throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        return bufferedReader.readLine();
+    }
+
+    public static int readInt() throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        return Integer.parseInt(bufferedReader.readLine());
+    }
+
+    public static double readDouble() throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        return Double.parseDouble(bufferedReader.readLine());
+    }
+
+    public static boolean readBoolean() throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        boolean result = bufferedReader.readLine().equals("true") ? true:false;
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
