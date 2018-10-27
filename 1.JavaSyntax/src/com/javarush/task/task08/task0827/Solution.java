1e142a9961fb18e588cca67faf225cb7c0bf8c25
Alexey <taler2002@gmail.com>
2018-10-27 17:06:25 +0300

    JavaRushTasks

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0827/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task08.task0827;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.Locale;
+
+/* 
+Работа с датой
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(isDateOdd("MAY 1 2013"));
+    }
+
+    public static boolean isDateOdd(String date) {
+        DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+                .parseCaseInsensitive()
+                .appendPattern("MMMM d yyyy")
+                .toFormatter(Locale.ENGLISH);
+        LocalDate outputDate = LocalDate.parse(date, formatter);
+        System.out.println(outputDate.getDayOfYear());
+        return (outputDate.getDayOfYear() % 2 != 0);
+    }
+}
