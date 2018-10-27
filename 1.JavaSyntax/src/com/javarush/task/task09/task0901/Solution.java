fe68be7d8e00630e1ac883ee8561177b24293cce
Alexey <taler2002@gmail.com>
2018-10-28 02:14:38 +0300

    JavaRushTasks

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0901/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task09.task0901;
+
+/* 
+Возвращаем StackTrace
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        method1();
+    }
+
+    public static StackTraceElement[] method1() {
+        method2();
+        StackTraceElement[] stackTraceElement = Thread.currentThread().getStackTrace();
+        return stackTraceElement;
+    }
+
+    public static StackTraceElement[] method2() {
+        method3();
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        return stackTraceElements;
+    }
+
+    public static StackTraceElement[] method3() {
+        method4();
+        return Thread.currentThread().getStackTrace();
+    }
+
+    public static StackTraceElement[] method4() {
+        method5();
+        return Thread.currentThread().getStackTrace();
+    }
+
+    public static StackTraceElement[] method5() {
+        return Thread.currentThread().getStackTrace();
+    }
+}
