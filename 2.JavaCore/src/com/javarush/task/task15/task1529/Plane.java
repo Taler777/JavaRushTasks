7cc0ca6ee5799223f76d2ff0765fb686302e44cb
Alexey <taler2002@gmail.com>
2018-11-09 01:57:58 +0300

    JavaRushTasks

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1529/Plane.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task15.task1529;
+
+public class Plane implements CanFly {
+    int amount;
+
+    public Plane(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
