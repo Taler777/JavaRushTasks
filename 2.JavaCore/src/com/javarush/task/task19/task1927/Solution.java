e18fc7339f750845685fcc5b012d5d5695c6f13a
Alexey <taler2002@gmail.com>
2018-11-20 00:28:22 +0300

    JavaRushTasks

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1927/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task19.task1927;
+
+/* 
+Контекстная реклама
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream consoleStream = System.out;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
+        System.setOut(printStream);
+        testString.printSomething();
+        String[] str = byteArrayOutputStream.toString().split("\n");
+        System.setOut(consoleStream);
+        for (int i = 0; i < str.length; i++) {
+            if (i > 0 && i % 2 == 0) System.out.print("JavaRush - курсы Java онлайн\n");
+            System.out.print(str[i]+"\n");
+        }
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("first");
+            System.out.println("second");
+            System.out.println("third");
+            System.out.println("fourth");
+            System.out.println("fifth");
+        }
+    }
+}
